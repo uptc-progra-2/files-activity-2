fd04d423863ceeccf0d4da0ba26e3fddd3cafeeb
Lunna SoSa <lunna.sosa@uptc.edu.co>
2023-03-07 11:40:22 -0500

    Inicio primer punto

--- src/co/edu/uptc/activity/two/first/FirstActivity.java
+++ src/co/edu/uptc/activity/two/first/FirstActivity.java
@@ -1,10 +1,23 @@
 package co.edu.uptc.activity.two.first;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
 public class FirstActivity {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try (DataInputStream dis=new DataInputStream(new FileInputStream("C:\\Users\\pc\\Desktop\\UPTC\\TERCER SEMESTRE\\PROGRAMACIÓN II\\files-activity-2\\src\\resources\\input\\texto.txt"));){
+			System.out.println("Datos en el archivo");
+			//Lee el primer byte
+			int ch=dis.read();
+			while (ch !=-1) {
+				System.out.println(ch);
+				//Lee el siguiente byte del archivo
+				ch=dis.read();
+			}
+			dis.close();
+		} catch (Exception e) {
+			e.getStackTrace();
+		} 
 	}
-
 }
